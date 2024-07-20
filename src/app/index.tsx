fb83b0325ebc2d814185a5367409d51ec8041330
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-07-20 02:34:40 -0300

    feat(tailwind): implment and config tailwind and assets

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+export default function Index() {
+  return (
+    <View className='flex-1 bg-primary-300 justify-center items-center'>
+      <Text>
+        Hello World!
+      </Text>
+    </View>
+  )
+}
---
+++ src/components/loading.tsx
@@ -0,0 +1,5 @@
+import { ActivityIndicator } from "react-native";
+
+export default function Loading() {
+  return <ActivityIndicator className="flex-1 bg-white-100 items-center justify-center text-lime-300" />
+}
